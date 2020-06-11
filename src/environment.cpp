4e1fdca81a1407d83ea28d09804076f48ab93200
Juncong <feijuncong@gmail.com>
2020-06-11 14:58:58 +0000

    Added bounding box rendering

--- src/environment.cpp
+++ src/environment.cpp
@@ -67,6 +67,11 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
         std::cout << "cluster size:";
         pointProcessor->numPoints(cluster);
         renderPointCloud(viewer, cluster, "obstCloud"+std::to_string(clusterId),colors[clusterId%colors.size()]);
+
+        // Render bounding box
+        Box box = pointProcessor->BoundingBox(cluster);
+        renderBox(viewer, box, clusterId, colors[clusterId%colors.size()]);
+
         ++clusterId;
     }
 }
